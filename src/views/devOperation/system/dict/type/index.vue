5c34052c8ad7d5e29165b9024907a18e775b84a1
tangyh <306479353@qq.com>
2021-10-17 21:02:12 +0800

    feat: 完善新版字典

---
+++ src/views/devOperation/system/dict/type/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="bg-white m-4 mr-2 overflow-hidden">
+    <BasicTable @register="registerTable">
+      <template #toolbar>
+        <a-button type="primary" color="error" @click="handleBatchDelete">{{
+          t('common.title.delete')
+        }}</a-button>
+        <a-button type="primary" @click="handleAdd">{{ t('common.title.add') }}</a-button>
+        <a-button type="primary" @click="handleEdit">{{ t('common.title.edit') }}</a-button>
+      </template>
+      <template #state="{ record }">
+        <Tag :color="record.state ? 'success' : 'error'">
+          {{ record.state ? t('lamp.common.enable') : t('lamp.common.disable') }}
+        </Tag>
+      </template>
+    </BasicTable>
+    <EditModal @register="registerDrawer" @success="handleSuccess" />
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { Tag } from 'ant-design-vue';
+  import { useI18n } from '/@/hooks/web/useI18n';
+  import { useRouter } from 'vue-router';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import { BasicTable, useTable } from '/@/components/Table';
+  import { useDrawer } from '/@/components/Drawer';
+  import { handleFetchParams } from '/@/utils/lamp/common';
+  import { ActionEnum } from '/@/enums/commonEnum';
+  import { page, remove } from '/@/api/devOperation/system/defDict';
+  import { columns, searchFormSchema } from './defDict.data';
+  import EditModal from './Edit.vue';
+  import { RouteEnum } from '/@/enums/biz/tenant';
+
+  export default defineComponent({
+    // 若需要开启页面缓存，请将此参数跟菜单名保持一致
+    name: 'DefDictManagement',
+    components: { BasicTable, EditModal, Tag },
+    emits: ['select'],
+    setup(_, { emit }) {
+      const { t } = useI18n();
+      const { createMessage, createConfirm } = useMessage();
+      // 编辑页弹窗
+      const [registerDrawer, { openDrawer }] = useDrawer();
+      const { replace } = useRouter();
+
+      // 表格
+      const [registerTable, { reload, getSelectRowKeys, getSelectRows }] = useTable({
+        title: t('devOperation.system.defDict.table.title'),
+        api: page,
+        columns: columns(),
+        formConfig: {
+          labelWidth: 70,
+          schemas: searchFormSchema(),
+        },
+        beforeFetch: handleFetchParams,
+        useSearchForm: true,
+        showTableSetting: false,
+        bordered: true,
+        rowKey: 'id',
+        rowSelection: {
+          type: 'radio',
+          columnWidth: 40,
+          onChange: (_, selectedRows: Recordable[]) => {
+            emit('select', selectedRows[0]);
+          },
+        },
+      });
+
+      // 弹出新增页面
+      function handleAdd() {
+        openDrawer(true, {
+          type: ActionEnum.ADD,
+        });
+      }
+
+      // 弹出编辑页面
+      function handleEdit() {
+        const rows = getSelectRows();
+        if (!rows || rows.length <= 0) {
+          createMessage.warning(t('common.tips.pleaseSelectTheData'));
+          return;
+        }
+
+        openDrawer(true, {
+          record: rows[0],
+          type: ActionEnum.EDIT,
+        });
+      }
+
+      // 新增或编辑成功回调
+      function handleSuccess() {
+        reload();
+      }
+
+      async function batchDelete(ids: any[]) {
+        await remove(ids);
+        createMessage.success(t('common.tips.deleteSuccess'));
+        handleSuccess();
+      }
+
+      // 点击单行删除
+      function handleDelete(record: Recordable, e: Event) {
+        e?.stopPropagation();
+        if (record?.id) {
+          batchDelete([record.id]);
+        }
+      }
+
+      // 点击批量删除
+      function handleBatchDelete() {
+        const ids = getSelectRowKeys();
+        if (!ids || ids.length <= 0) {
+          createMessage.warning(t('common.tips.pleaseSelectTheData'));
+          return;
+        }
+        createConfirm({
+          iconType: 'warning',
+          content: t('common.tips.confirmDelete'),
+          onOk: async () => {
+            await batchDelete(ids);
+          },
+        });
+      }
+
+      // 查看字典项
+      function handleViewItem(record: Recordable, e: Event) {
+        e.stopPropagation();
+        replace({
+          name: RouteEnum.DICT_ITEM,
+          params: { dictId: record.id },
+          query: { name: record.name, key: record.key, remark: record.remark },
+        });
+      }
+
+      return {
+        t,
+        registerTable,
+        registerDrawer,
+        handleAdd,
+        handleEdit,
+        handleDelete,
+        handleSuccess,
+        handleBatchDelete,
+        handleViewItem,
+      };
+    },
+  });
+</script>
