b0e0c3fede8af2ca7e5f509e37318b33cd99fa67
zuihou <244387066@qq.com>
2021-11-02 08:15:22 +0800

    feat: 新增头像预览组件

--- src/views/lamp/home/components/WorkbenchHeader.vue
+++ src/views/lamp/home/components/WorkbenchHeader.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="lg:flex">
-    <Avatar :src="userinfo.avatar || headerImg" :size="72" class="!mx-auto !block" />
+    <AvatarPreview
+      :fileId="userinfo.avatarId"
+      :errorTxt="userinfo?.nickName?.substr(0, 2)"
+      :size="72"
+      class="!mx-auto !block"
+    />
     <div class="md:ml-6 flex flex-col justify-center md:mt-0 mt-2">
       <h1 class="md:text-lg text-md">早安, {{ userinfo.nickName }}, 开始您一天的工作吧！</h1>
       <span class="text-secondary"> {{ userinfo?.workDescribe ?? '这家伙很懒什么也没留下' }} </span>
@@ -24,9 +29,8 @@
 </template>
 <script lang="ts" setup>
   import { computed } from 'vue';
-  import { Avatar } from 'ant-design-vue';
+  import { AvatarPreview } from '/@/components/AvatarPreview';
   import { useUserStore } from '/@/store/modules/user';
-  import headerImg from '/@/assets/images/header.jpg';
 
   const userStore = useUserStore();
   const userinfo = computed(() => userStore.getUserInfo);
