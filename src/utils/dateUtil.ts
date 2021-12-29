f24e62db11603cd40dd5a188526cee0e1eccc598
zuihou <244387066@qq.com>
2021-12-29 22:55:58 +0800

    feat: 新增格式化日期方法

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -8,6 +8,7 @@ dayjs.extend(duration);
 
 const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const DATE_FORMAT = 'YYYY-MM-DD';
+const TIME_FORMAT = 'HH:mm:ss';
 
 export function formatToDateTime(
   date: dayjs.Dayjs | undefined = undefined,
@@ -19,6 +20,13 @@ export function formatToDateTime(
 export function formatToDate(
   date: dayjs.Dayjs | undefined = undefined,
   format = DATE_FORMAT,
+): string {
+  return dayjs(date).format(format);
+}
+
+export function formatToTime(
+  date: dayjs.Dayjs | undefined = undefined,
+  format = TIME_FORMAT,
 ): string {
   return dayjs(date).format(format);
 }
