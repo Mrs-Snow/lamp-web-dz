26b363c17f7681f6ed3aed1565d3323d5e78ab0b
zuihou <244387066@qq.com>
2022-04-22 18:07:46 +0800

    refactor(BasicTitle): 支持 line、dashed、cursor、noneSelect等属性

--- src/components/Basic/src/BasicTitle.vue
+++ src/components/Basic/src/BasicTitle.vue
@@ -9,6 +9,7 @@
   import { useSlots, computed } from 'vue';
   import BasicHelp from './BasicHelp.vue';
   import { useDesign } from '/@/hooks/web/useDesign';
+  import { propTypes } from '/@/utils/propTypes';
 
   const props = defineProps({
     /**
@@ -23,12 +24,24 @@
      * Whether the color block on the left side of the title
      * @default: false
      */
-    span: { type: Boolean },
+    span: propTypes.bool.def(false),
     /**
      * Whether to default the text, that is, not bold
      * @default: false
      */
-    normal: { type: Boolean },
+    normal: propTypes.bool.def(true),
+    /**
+     * 是否显示底部线条
+     */
+    line: propTypes.bool.def(false),
+    /**
+     * 底部线条 是否虚线
+     */
+    dashed: propTypes.bool.def(false),
+    /** 是否显示光标 */
+    cursor: propTypes.bool.def(false),
+    /** 是否不能选择 */
+    noneSelect: propTypes.bool.def(false),
   });
 
   const { prefixCls } = useDesign('basic-title');
@@ -36,7 +49,11 @@
   const getClass = computed(() => [
     prefixCls,
     { [`${prefixCls}-show-span`]: props.span && slots.default },
-    { [`${prefixCls}-normal`]: props.normal },
+    { [`${prefixCls}-line`]: !!props.line },
+    { [`${prefixCls}-cursor`]: !!props.cursor },
+    { [`${prefixCls}-none-select`]: !!props.noneSelect },
+    { [`${prefixCls}-dashed`]: !!props.line && !!props.dashed },
+    { [`${prefixCls}-normal`]: !!props.normal },
   ]);
 </script>
 <style lang="less" scoped>
@@ -46,16 +63,35 @@
     position: relative;
     display: flex;
     padding-left: 7px;
-    font-size: 16px;
+    font-size: @font-size-lg;
     font-weight: 500;
     line-height: 24px;
     color: @text-color-base;
-    cursor: pointer;
-    user-select: none;
+
+    &-none-select {
+      user-select: none;
+    }
+
+    &-cursor {
+      cursor: pointer;
+    }
 
     &-normal {
-      font-size: 14px;
-      font-weight: 500;
+      font-size: @font-size-base;
+      font-weight: normal;
+    }
+
+    &-line {
+      border-bottom: @border-width-base solid transparent;
+      // Chrome not accept `inherit` in `border-top`
+      border-bottom-color: inherit;
+    }
+
+    &-dashed {
+      background: none;
+      border-color: @divider-color;
+      border-style: dashed;
+      border-width: 0 0 @border-width-base;
     }
 
     &-show-span::before {
@@ -63,7 +99,7 @@
       top: 4px;
       left: 0;
       width: 3px;
-      height: 16px;
+      height: @font-size-lg;
       margin-right: 4px;
       background-color: @primary-color;
       content: '';
